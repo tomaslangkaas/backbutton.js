af8f81fe3ce4915fe140fd582f45dcec266d5974
tomaslangkaas <tomaslangkaas@users.noreply.github.com>
2018-02-09 18:11:29 +0100

    Update backbutton.js

--- backbutton.js
+++ backbutton.js
@@ -1,1 +1,130 @@
-var backbutton = function(b){function k(a){a=a||b[d][m]||"";var c=a.indexOf("#");return-1<c?a.slice(c+1):""}function n(a,c){(c||b[d]).hash="#"+a}function v(){var a=k(),c=k(e[d][m]);a!=l?(a!=c&&(g=e[p],g.open(),g.close(),n(a,e[d])),q(0,a),l=a):c!=l&&(b[d][m]=b[d][m].replace(/#.*/,"#")+c)}function q(a,c){var b,f=r.length;c=c||k();t&&t(c);for(b=0;b<f;b++)r[b](c)}var r=[],p="document",u=b[p],d="location",m="href",h="onhashchange"in b&&b.addEventListener,e,g,t,l=k(),w={observe:function(a){r.push(a)},navigate:n,current:k,routes:function(a,c){t=a?function(b){for(var f=0,d=a.length,e;f<d;f+=2)if(e=b.match(a[f])){a[f+1].apply(null,e.slice(1));break}f==d&&c&&c(b)}:0},refresh:q,back:function(){b.history.back()}};n(l);h?b.addEventListener("hashchange",q,!1):(h=u.createElement("iframe"),h.src="javascript:0",h.style.display="none",h.tabIndex=-1,e=u.body.insertBefore(h,u.body.firstChild).contentWindow,g=e[p],g.open(),g.close(),n(l,e[d]),setInterval(v,50));return w}(window);
+(function(window) {
+
+  var listeners = [false],
+    document = "document",
+    location = "location",
+    href = "href",
+    lastFragment = getFragment(),
+    winDocument = window[document],
+    hashChangeSupport = "onhashchange" in window && window.addEventListener,
+    // public interface
+
+    backbuttonObject = (window["backbutton"] = {
+      version: "1.0.0",
+
+      observe: function(fn) {
+        var i = listeners.length;
+        listeners[i] = fn;
+        return function() {
+          listeners[i] = false;
+        };
+      },
+
+      navigate: setFragment,
+
+      current: getFragment,
+
+      routes: function(routingTable, autoStart) {
+        var oldRouter = listeners[0];
+        if (autoStart === void 0 || autoStart === true) setTimeout(notify);
+        if (routingTable && typeof routingTable === "object") {
+          listeners[0] = function(fragmentString) {
+            for (
+              var index = 0,
+                length = routingTable.length,
+                matchResult,
+                returnValue;
+              index < length;
+              index += 2
+            ) {
+              if ((matchResult = fragmentString.match(routingTable[index]))) {
+                returnValue = routingTable[index + 1].apply(
+                  backbuttonObject,
+                  matchResult.slice(1)
+                );
+                if (!returnValue) {
+                  break;
+                } else if (typeof returnValue === "string") {
+                  fragmentString = returnValue;
+                }
+              }
+            }
+          };
+        } else {
+          listeners[0] = routingTable;
+        }
+        return oldRouter;
+      },
+
+      refresh: notify
+    });
+
+  // backbutton initialization and setup
+
+  setFragment(lastFragment);
+  
+  if (hashChangeSupport) {
+    window.addEventListener("hashchange", notify, false);
+
+  /* start of old browser support */
+  } else {
+    var pollInterval = 50,
+      iFrame,
+      iWindow,
+      iWinDocument;
+
+    iFrame = winDocument.createElement("iframe");
+    iFrame.src = "javascript:0";
+    iFrame.style.display = "none";
+    iFrame.tabIndex = -1;
+    iWindow = winDocument.body.insertBefore(iFrame, winDocument.body.firstChild)
+      .contentWindow;
+    iWinDocument = iWindow[document];
+    iWinDocument.open();
+    iWinDocument.close();
+    setFragment(lastFragment, iWindow[location]);
+    setInterval(function() {
+      var current = getFragment(),
+        last = getFragment(iWindow[location][href]);
+
+      if (current != lastFragment) {
+        if (current != last) {
+          iWinDocument = iWindow[document];
+          iWinDocument.open();
+          iWinDocument.close();
+          setFragment(current, iWindow[location]);
+        }
+        notify(0, current);
+        lastFragment = current;
+      } else if (last != lastFragment) {
+        window[location][href] =
+          window[location][href].replace(/#.*/, "#") + last;
+      }
+    }, pollInterval);
+  }
+  /* end of old browser support */
+
+  // internal functions
+
+  function getFragment(url) {
+    url = url || window[location][href] || "";
+    var i = url.indexOf("#");
+    return i > -1 ? url.slice(i + 1) : "";
+  }
+
+  function setFragment(fragment, locationObject) {
+    (locationObject || window[location]).hash = "#" + fragment;
+  }
+
+  function notify(eventObject, fragment) {
+    var index,
+      length = listeners.length;
+
+    fragment = unescape(fragment || getFragment());
+
+    for (index = 0; index < length; index++)
+      typeof listeners[index] === "function" &&
+        listeners[index].call(backbuttonObject, fragment);
+  }
+
+})(window);
