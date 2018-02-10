547c711b8af02aa0501999ffa2dbe5adfa0adf09
tomaslangkaas <tomaslangkaas@users.noreply.github.com>
2018-02-10 23:01:27 +0100

    Update backbutton.js

--- backbutton.js
+++ backbutton.js
@@ -10,7 +10,7 @@
     // public interface
 
     backbuttonObject = (window["backbutton"] = {
-      version: "1.0.0",
+      version: "1.0.1",
 
       observe: function(fn) {
         var i = listeners.length;
